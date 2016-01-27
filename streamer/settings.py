680d94d8c445aa00e01a64c33af94c3c0006d03d
onurmatik <omat@gezgin.com>
2016-01-27 17:31:27 +0200

    configuration arrangements

--- streamer/settings.py
+++ streamer/settings.py
@@ -79,32 +79,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'streamer.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'local.db'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
+try:
+    from settings_local import *
+except ImportError:
+    # no extra settings is required for stream listener installations
+    pass
